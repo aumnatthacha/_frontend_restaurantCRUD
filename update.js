3c27c276bb729a882780e90e28584ec5550b062c
aumnatthacha <96460324+aumnatthacha@users.noreply.github.com>
2023-08-21 08:58:09 +0700

    2

--- update.js
+++ update.js
@@ -1,56 +1,22 @@
-// const updateRestaurant = async () => {
+// const editRestaurant = async () => {
 //     const id = document.getElementById("restaurantId").value;
-//     const name = document.getElementById("name").value;
-//     const type = document.getElementById("type").value;
-//     const img = document.getElementById("img").value;
 
-//     if (id && name && type && img) {
-//         const params = {
-//             name: name,
-//             type: type,
-//             img: img
-//         };
+//     if (id) {
 //         try {
-//             const restaurant = await fetch(`http://localhost:5000/res/${id}`, {
-//                 method: "PUT",
-//                 mode: "cors",
-//                 cache: "no-cache",
-//                 credentials: "same-origin",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify(params)
-//             }).then((response) => {
-//                 return response.json();
-//             }).then((res) => {
-//                 alert("Restaurant with ID " + res.id + " updated successfully");
-//             });
+//             const response = await fetch(`http://localhost:5000/res/${id}`);
+//             const restaurantData = await response.json();
+
+//             document.getElementById("name").value = restaurantData.name;
+//             document.getElementById("type").value = restaurantData.type;
+//             document.getElementById("img").value = restaurantData.img;
+
 //         } catch (error) {
-//             alert("Unable to update the restaurant menu");
+//             alert("อัปเดตสำเร็จ");
 //         }
 //     } else {
-//         alert("All fields are required");
+//         alert("Restaurant ID is required");
 //     }
 // };
-const editRestaurant = async () => {
-    const id = document.getElementById("restaurantId").value;
-
-    if (id) {
-        try {
-            const response = await fetch(`http://localhost:5000/res/${id}`);
-            const restaurantData = await response.json();
-
-            document.getElementById("name").value = restaurantData.name;
-            document.getElementById("type").value = restaurantData.type;
-            document.getElementById("img").value = restaurantData.img;
-
-        } catch (error) {
-            alert("Unable to fetch restaurant data");
-        }
-    } else {
-        alert("Restaurant ID is required");
-    }
-};
 
 const updateRestaurant = async () => {
     const id = document.getElementById("restaurantId").value;
@@ -78,16 +44,20 @@ const updateRestaurant = async () => {
             });
 
             const updatedData = await updateResponse.json();
-            alert("Restaurant with ID " + updatedData.id + " updated successfully");
+            alert("อัปเดตสำเร็จ");
+
+            // Redirect to the main page after successful update
+            window.location.href = "index.html"; // Replace with your main page URL
+
         } catch (error) {
-            alert("Unable to update the restaurant menu");
+            alert("Failed to update restaurant");
         }
     } else {
         alert("All fields are required");
     }
 };
 
-// Attach the editRestaurant function to a "Edit Restaurant" button
-const editButton = document.getElementById("editButton");
-editButton.addEventListener("click", editRestaurant);
 
+// Attach the editRestaurant function to a "Edit Restaurant" button
+// const editButton = document.getElementById("editButton");
+// editButton.addEventListener("click", editRestaurant);
